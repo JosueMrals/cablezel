71ea069713cdb2e8626094db6ff9fa74cbf1071e
POXS44 <vzeledon7@gmail.com>
2022-07-14 12:49:25 -0600

    Creacion de la clase para manipulacion de fichero de reportes

---
+++ src/main/java/com/josue/reportes/Reportes.java
@@ -0,0 +1,43 @@
+package com.josue.reportes;
+
+
+
+
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+//import javax.imageio.ImageIO;
+
+
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+
+
+
+
+public class Reportes {
+
+    static JasperPrint jp;
+
+
+
+}
