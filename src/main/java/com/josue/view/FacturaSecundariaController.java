14b1909689cda274cbbd8b1095d4cef47e20f977
yessermiranda13 <yesser.miranda@uraccan.edu.ni>
2022-10-26 22:18:21 -0600

    Completando el pago en la facturacion secundaria - se evita el error de obtencion de indice fuera de rango - se genera los pagos en la facturacion secundaria de comprobacion

--- src/main/java/com/josue/view/FacturaSecundariaController.java
+++ src/main/java/com/josue/view/FacturaSecundariaController.java
@@ -3,13 +3,21 @@ package com.josue.view;
 import com.josue.modelo.Contrato;
 import com.josue.modelo.DetalleFactura;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FacturaSecundariaController implements Initializable {
+
+     // logger de Log4j
+    private static final Logger logger = LogManager.getLogger(FacturaSecundariaController.class);
 
     public TableView<DetalleFactura> tvMostrarPagos;
     public TableColumn<DetalleFactura, String> colN1;
@@ -18,6 +26,8 @@ public class FacturaSecundariaController implements Initializable {
     public TableColumn<DetalleFactura, String > colN4;
     public TableColumn<DetalleFactura, String > colN5;
     public Button btFacturar;
+
+    public Button btImprimir;
     public TextArea txtDireccion;
     public TextField txtCliente;
     FacturarController facturarController;
@@ -28,9 +38,23 @@ public class FacturaSecundariaController implements Initializable {
     }
 
     public void recibirDatos(FacturarController facturarPrincipalController , ObservableList<DetalleFactura> datos){
+        logger.info("Recibiendo datos de la factura principal");
+        logger.info("Datos recibidos: " + datos);
         tvMostrarPagos.setItems(datos);
         this.facturarController = facturarPrincipalController;
+
     }
 
 
+    public void btCompletarClick(ActionEvent actionEvent) {
+        // pagar
+        facturarController.realizarPago();
+        // cerrar la ventana actual
+        Node source = (Node) actionEvent.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
+
+    public void btImprimirClick(ActionEvent actionEvent) {
+    }
 }
